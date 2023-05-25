3356c45ded88e80fe30a80d07b7525107a6ed6fe
benjamin <benjamin@exposebox.com>
2023-05-25 21:51:06 +0300

    - initial commit;

---
+++ src/components/DraggableList.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { DragDropContext, Draggable } from 'react-beautiful-dnd';
+import {StrictModeDroppable as Droppable} from './StrictModeDroppable';
+import {List, ListItem, ListItemButton, ListItemText} from "@mui/material";
+
+const DraggableList = ({initialItems, onChange, fieldName}) => {
+    const [items, setItems] = useState(initialItems);
+
+    useEffect(() => {
+        setItems(initialItems);
+    }, [initialItems]);
+
+    const handleDragEnd = (result) => {
+        if (!result.destination) return;
+
+        const updatedItems = [...items];
+        const [removed] = updatedItems.splice(result.source.index, 1);
+        updatedItems.splice(result.destination.index, 0, removed);
+
+        setItems(updatedItems);
+
+        onChange(updatedItems, fieldName);
+    };
+
+    return (
+        <DragDropContext onDragEnd={handleDragEnd}>
+            <Droppable droppableId="draggable-list">
+                {(provided) => (
+                    <List {...provided.droppableProps} ref={provided.innerRef}>
+                        {items.map((item, index) => (
+                            <Draggable key={item.id} draggableId={item.id} index={index}>
+                                {(provided) => (
+                                    <ListItem
+                                        divider
+                                        dense
+                                        sx={{
+                                            bgcolor: `${item.isCorrect ? '#a2cf6e' : 'transparent'}`,
+                                        }}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        ref={provided.innerRef}
+                                    >
+                                        <ListItemButton>
+                                            <ListItemText primary={item.content}/>
+                                        </ListItemButton>
+                                    </ListItem >
+                                )}
+                            </Draggable>
+                        ))}
+                        {provided.placeholder}
+                    </List>
+                )}
+            </Droppable>
+        </DragDropContext>
+    );
+};
+
+export default DraggableList;
